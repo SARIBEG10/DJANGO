9a3b409e49f5dcd16ea944ce44e41c079c735de2
SARIBEG10 <sfarmanian@gmail.com>
2021-02-22 00:30:01 +0330

    Add detail template

--- food/views.py
+++ food/views.py
@@ -7,13 +7,18 @@ from django.template import loader
 
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
+    # template = loader.get_template('food/index.html')
     context = {
         'item_list': item_list,
     }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
 
 
 def items(request):
     return HttpResponse('<h1> This is item view </h1>')
 
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    return render(request, 'food/detail.html', {'item': item})
